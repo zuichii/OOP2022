53b4403f3b814a977a442e3e2155e75e76b5ee62
zuichii <ntc.cordero@gmail.com>
2022-08-12 13:40:17 +0930

    workshop3

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
-#
+#include <string>
+using namespace std;
 
 void print_class(std::string courses[4], std::string students[], int report_card[][4], int nstudents){
+    cout << "Report Card ";
+    for(int i = 0; i < 4; i++){
+        cout << courses[i];
+    }
+    cout << endl;
+
     
+
+    for(int i = 0; i < nstudents; i++){
+        
+
+
+    }
+    
+        
+        
+        
+        
+        for(int j = 0; j < 4; j++){
+
+        }
+    }
+    
+
+
+    
+
+
 
 
 }
