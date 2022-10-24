1a86985f610f670910b43085c16076cea3faf319
zuichii <ntc.cordero@gmail.com>
2022-10-24 11:51:14 +1030

    animal

---
+++ animal.cpp
@@ -0,0 +1,6 @@
+#include "animal.h"
+
+Animal::Animal(){
+
+};
+
---
+++ animal.h
@@ -0,0 +1,12 @@
+#define ANIMAL_H
+#ifndef ANIMAL_H
+#include <iostream>
+
+class Animal{
+    public:
+    Animal();
+
+
+
+};
+#endif
