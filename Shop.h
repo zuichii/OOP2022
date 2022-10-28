201e633a19f92f9a9223035e0a5790186ff9371c
zuichii <ntc.cordero@gmail.com>
2022-10-28 15:12:41 +1030

    make shop

---
+++ Shop.h
@@ -0,0 +1,27 @@
+#ifndef SHOP_H
+#define SHOP_H
+#include "Employee.h"
+#include "Casual.h"
+#include "Manager.h"
+
+
+class Shop{
+
+    private:
+        Shop();
+    
+    protected:
+        Employee** employees;
+        int count;
+
+    public:
+        void addEmployee(Employee *employee);
+        Employee **get_employees();
+        int get_count();
+
+};
+
+
+
+
+#endif
